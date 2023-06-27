bbb497ab53cecd0a56122f4d7a7bc76437f42457
dev-bre <anbrandi@gmail.com>
2023-06-27 22:16:07 +0100

    setup-srcipts

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './output.css';
-import { HomePage } from "./pages/homepage";
+import { HomePage } from "./pages/Homepage";
 
 function App() {
   return (
--- packages/ui/src/pages/Homepage.tsx
+++ packages/ui/src/pages/Homepage.tsx
@@ -1,4 +1,4 @@
 export const HomePage: React.FC = () => {
 
-    return(<></>);
+    return(<>hello</>);
 }
