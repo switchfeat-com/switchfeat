2f43acf0861a31c6b630cb12d37101a08384a142
dev-bre <anbrandi@gmail.com>
2023-07-09 17:24:38 +0100

    chore(ui): improve layout

--- packages/ui/src/components/Login.tsx
+++ packages/ui/src/components/Login.tsx
@@ -10,9 +10,7 @@ export const Login: React.FC = () => {
     const navigateTo = useNavigate();
 
     return (
-        <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
-
-
+        <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">   
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
                 <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
 
