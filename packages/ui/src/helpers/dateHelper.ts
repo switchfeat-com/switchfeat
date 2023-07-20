ee6eef667aa7bdbeb1b60a19367aff0fe8c9ffc4
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-07-21 00:37:29 +0530

    fix: import errors

--- packages/ui/src/helpers/dateHelper.ts
+++ packages/ui/src/helpers/dateHelper.ts
@@ -1,27 +1,31 @@
 import { DateTime } from "luxon";
 
 export const formatDateTime = (dateStr: string | undefined) => {
+	if (dateStr === undefined) {
+		return "";
+	}
 
-    if (dateStr === undefined) {
-        return "";
-    }
-
-    return DateTime.fromISO(dateStr).toFormat("MMM dd, yyyy @ hh:mm a");
+	return DateTime.fromISO(dateStr).toFormat("MMM dd, yyyy @ hh:mm a");
 };
 
 export const utcNow = (): DateTime => {
-    return DateTime.utc();
+	return DateTime.utc();
 };
 
-export const getDateWithOffset = (nrDays: number, start: DateTime): DateTime => {
-    return start.minus({ "days": nrDays });
+export const getDateWithOffset = (
+	nrDays: number,
+	start: DateTime
+): DateTime => {
+	return start.minus({ days: nrDays });
 };
 
 export const diffFromUtcInDays = (date: string | undefined) => {
+	if (date === undefined) {
+		return;
+	}
 
-    if (date === undefined) {
-        return;
-    }
-
-    return DateTime.fromISO(date).diff(DateTime.utc(), ["days"]).toObject().days?.toFixed(0);
+	return DateTime.fromISO(date)
+		.diff(DateTime.utc(), ["days"])
+		.toObject()
+		.days?.toFixed(0);
 };
