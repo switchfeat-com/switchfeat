8320ae296390e471354136283d66c4bccdf536d1
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-07-22 23:54:54 +0530

    notification provider

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,25 +1,49 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './output.css';
+import "./output.css";
 import { HomePage } from "./pages/Homepage";
 import { AppContextProvider } from "./context/AppContextProvider";
 import { Flags } from "./pages/Flags";
 import { Segments } from "./pages/Segments";
 import { Dashboard } from "./pages/Dashboard";
 import PrivateRoute from "./components/shared/PrivateRoute";
+import NotificationProvider from "./components/shared/NotificationProvider";
 
 const App: React.FC = () => {
-  return (
-    <AppContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-          <Route path="/flags" element={<PrivateRoute><Flags /></PrivateRoute>} />
-          <Route path="/segments" element={<PrivateRoute><Segments /></PrivateRoute>} />
-        </Routes>
-      </BrowserRouter>
-    </AppContextProvider>
-  );
+	return (
+		<AppContextProvider>
+			<NotificationProvider>
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route
+							path="/dashboard"
+							element={
+								<PrivateRoute>
+									<Dashboard />
+								</PrivateRoute>
+							}
+						/>
+						<Route
+							path="/flags"
+							element={
+								<PrivateRoute>
+									<Flags />
+								</PrivateRoute>
+							}
+						/>
+						<Route
+							path="/segments"
+							element={
+								<PrivateRoute>
+									<Segments />
+								</PrivateRoute>
+							}
+						/>
+					</Routes>
+				</BrowserRouter>
+			</NotificationProvider>
+		</AppContextProvider>
+	);
 };
 
 export default App;
---
+++ packages/ui/src/components/shared/NotificationProvider.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+
+type ToastType = 'success' | 'error' | 'loading' | 'blank' | 'custom';
+type ValueFunction<TValue, TArg> = (arg: TArg) => TValue;
+type ValueOrFunction<TValue, TArg> = TValue | ValueFunction<TValue, TArg>;
+type Renderable = JSX.Element | string | null;
+interface Toast {
+	type: ToastType;
+	id: string;
+	message: ValueOrFunction<Renderable, Toast>;
+	icon?: Renderable;
+	duration?: number;
+	pauseDuration: number;
+	position?: ToastPosition;
+	ariaProps: {
+		role: 'status' | 'alert';
+		'aria-live': 'assertive' | 'off' | 'polite';
+	};
+	style?: React.CSSProperties;
+	className?: string;
+	iconTheme?: IconTheme;
+	createdAt: number;
+	visible: boolean;
+	height?: number;
+}
+
+type ToastOptions = Partial<
+	Pick<
+		Toast,
+		| 'id'
+		| 'icon'
+		| 'duration'
+		| 'ariaProps'
+		| 'className'
+		| 'style'
+		| 'position'
+		| 'iconTheme'
+	>
+>;
+type DefaultToastOptions = ToastOptions & {
+	[key in ToastType]?: ToastOptions;
+};
+type ToastPosition =
+	| 'top-left'
+	| 'top-center'
+	| 'top-right'
+	| 'bottom-left'
+	| 'bottom-center'
+	| 'bottom-right';
+
+interface IconTheme {
+	primary: string;
+	secondary: string;
+}
+
+interface NotificationProviderProps {
+	position?: ToastPosition;
+	toastOptions?: DefaultToastOptions;
+	reverseOrder?: boolean;
+	gutter?: number;
+	containerStyle?: React.CSSProperties;
+	containerClassName?: string;
+	children?: React.ReactNode;
+}
+
+const NotificationProvider = ({
+	children,
+	position,
+	toastOptions,
+	reverseOrder,
+	gutter,
+	containerStyle,
+	containerClassName,
+}: NotificationProviderProps) => {
+	return (
+		<>
+			<Toaster
+				position={position}
+				toastOptions={toastOptions}
+				reverseOrder={reverseOrder}
+				gutter={gutter}
+				containerStyle={containerStyle}
+				containerClassName={containerClassName}
+			/>
+			{children}
+		</>
+	);
+};
+
+export default NotificationProvider;
