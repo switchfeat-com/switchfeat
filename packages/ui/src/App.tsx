e3cde214600c09e8e419c2f583eb6f39560fe8c2
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-08-13 18:21:43 +0530

    feat: Binded all routes with DashboardLayout

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -4,12 +4,11 @@ import { HomePage } from "./pages/Homepage";
 import { AppContextProvider } from "./context/AppContextProvider";
 import { Flags } from "./pages/Flags";
 import { Segments } from "./pages/Segments";
-import { Dashboard } from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./components/shared/PrivateRoute";
 import { NotificationProvider } from "./components/shared/NotificationProvider";
 import { ApiKeys } from "./components/settings/apiKeys/ApiKeys";
-import Dashboard1 from "./pages/Dashboard1";
-import AuthLayout from "./components/shared/AuthLayout";
+import DashboardLayout from "./layout/DashboardLayout";
 
 const App: React.FC = () => {
     return (
@@ -18,42 +17,39 @@ const App: React.FC = () => {
                 <BrowserRouter>
                     <Routes>
                         <Route path="/" element={<HomePage />} />
-                        <Route
-                            path="/dashboard"
-                            element={
-                                <PrivateRoute>
-                                    <Dashboard />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path="/flags"
-                            element={
-                                <PrivateRoute>
-                                    <Flags />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path="/segments"
-                            element={
-                                <PrivateRoute>
-                                    <Segments />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path="/settings/apikeys"
-                            element={
-                                <PrivateRoute>
-                                    <ApiKeys />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route element={<AuthLayout />}>
+
+                        <Route element={<DashboardLayout />}>
                             <Route
-                                path="/dashboard1"
-                                element={<Dashboard1 />}
+                                path="/dashboard"
+                                element={
+                                    <PrivateRoute>
+                                        <Dashboard />
+                                    </PrivateRoute>
+                                }
+                            />
+                            <Route
+                                path="/flags"
+                                element={
+                                    <PrivateRoute>
+                                        <Flags />
+                                    </PrivateRoute>
+                                }
+                            />
+                            <Route
+                                path="/segments"
+                                element={
+                                    <PrivateRoute>
+                                        <Segments />
+                                    </PrivateRoute>
+                                }
+                            />
+                            <Route
+                                path="/settings/apikeys"
+                                element={
+                                    <PrivateRoute>
+                                        <ApiKeys />
+                                    </PrivateRoute>
+                                }
                             />
                         </Route>
                     </Routes>
