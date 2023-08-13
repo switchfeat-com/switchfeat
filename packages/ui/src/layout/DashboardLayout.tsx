3aa0a88cf7d0a1a2a680847aa78f1064de77d64c
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-08-13 20:46:53 +0530

    feat: sidebar resposive

--- packages/ui/src/layout/DashboardLayout.tsx
+++ packages/ui/src/layout/DashboardLayout.tsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../components/shared/Sidebar";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/20/solid";
 
 interface DashboardLayoutProps {
     children: React.ReactNode;
 }
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+    const [sidebar, setSidebar] = useState<boolean>(false);
     return (
         <div className="flex w-screen bg-[#F9FAFB]">
-            <aside className="pl-2 py-2 w-60 h-screen">
-                <div className="pl-2 pt-10 rounded-2xl bg-white h-full">
+            <Bars3Icon
+                className="w-6 h-6 absolute top-4 left-4 cursor-pointer text-[#2563EB] block md:hidden"
+                onClick={() => setSidebar(!sidebar)}
+            />
+            <aside
+                className={`pl-2 py-2 w-60 h-screen z-50 ${
+                    sidebar ? "block fixed top-0" : "hidden"
+                } md:block`}
+            >
+                <div className="pl-2 pt-10 rounded-2xl bg-white h-full relative">
+                    {sidebar ? (
+                        <XMarkIcon
+                            className="w-4 absolute top-5 cursor-pointer right-3"
+                            onClick={() => setSidebar(false)}
+                        />
+                    ) : (
+                        ""
+                    )}
                     <Sidebar />
                 </div>
             </aside>
-            <div className="w-[calc(100vw_-_240px)] p-6 max-w-6xl mx-auto">
+            <div
+                className={`w-screen md:w-[calc(100vw_-_240px)] p-6 max-w-6xl mx-auto pt-16 md:pt-6 ${
+                    sidebar ? "opacity-10" : ""
+                }`}
+            >
                 {children}
             </div>
         </div>
